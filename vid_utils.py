3ffde5203b0b73629bf808bd191d3712401deafd
Marco Nie <nie11kun@outlook.com>
2019-10-17 12:43:34 +0800

    update

--- vid_utils.py
+++ vid_utils.py
@@ -71,8 +71,7 @@ class Video:
             if "[download] Destination:" in line:
                 self.file_name = line[24:] # name of the file
             elif "has already been downloaded" in line:
-                self.file_name = line[11:]
-                self.file_name.replace(' has already been downloaded','')
+                self.file_name = line[11:-28]
 
     def check_dimension(self):
         if os.path.getsize(self.file_name) > 50 * 1024 * 1023:# big than 50mb
@@ -91,28 +90,3 @@ class Video:
         files = glob.glob(self.file_name + '*')
         for f in files: #removing old files
             os.remove(f)
-
-
-
-
-#__________________________OLD STUFFS, TOUCH CAREFULLY__________________________
-
-# this is the soft-split version, require avconv, but the audio isn't synchronized, avconv's problems :(
-'''
-def get_duration(filepath): # get duration in seconds
-    cmd = "avconv -i %s" % filepath
-    p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    di = p.communicate()
-    for line in di:
-        if line.rfind(b"Duration") > 0:
-            duration = str(re.findall(b"Duration: (\d+:\d+:[\d.]+)", line)[0])
-    return 3600 * int(duration[2: 4]) + 60 * int(duration[5: 7]) + int(duration[8: 10])
-
-def check_dimension(f): # if f is bigger than 50MB split it in subvideos
-    if os.path.getsize(f) > 50 * 1024 * 1023:
-        duration = get_duration(f)
-        for i in range(0, duration, 180):
-            start = strftime("%H:%M:%S", strptime('{0} {1} {2}'.format(i // 3600, (i // 60) % 60, i % 60), "%H %M %S")) # TODO this is  not pythonic code!
-            os.system("""avconv -i '{0}' -vcodec copy -acodec copy -ss {1} -t {2} 'part_{3}.mp4'""".format(f, start, 180, (i // 180) % 180))
-        os.remove(f) # delete original file
-'''
